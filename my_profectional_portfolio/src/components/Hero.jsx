d202045c53c65935ce441524709ba6df7c6adf5a
sashik mindaka <mindakaofficial0069@gmail.com>
2026-09-03 21:09:34 +0530

    add photos to hero section

---
+++ my_profectional_portfolio/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import myimg from 
+
+export default function Hero() {
+  return (
+    <section>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        Sashik Mindaka
+      </div>
+    </section>
+  )
+}
