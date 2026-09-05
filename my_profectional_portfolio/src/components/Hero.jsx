b6c5174225beeee769ccd603335a94b7f5db452c
sashik mindaka <mindakaofficial0069@gmail.com>
2026-09-05 13:36:44 +0530

    add my photo and creating hero section

--- my_profectional_portfolio/src/components/Hero.jsx
+++ my_profectional_portfolio/src/components/Hero.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import myimg1 from '../assests/myimg1.jpg';
+import myimg1 from '../assests/Untitled_design__13_-removebg-preview.png';
 
 
 export default function Hero() {
   return (
-    <section>
-      <div className='p-7'>
+    <section className='bg-amber-600'>
+
+      <div className='w-640 h-64 lg:w-1030 lg:h-96'>
         <img src={myimg1} alt="my image 1" />
+        <h1>i'm sashik mindaka</h1>
       </div>
     </section>
   )
