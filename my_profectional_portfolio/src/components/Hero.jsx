286f2817359dd625275e0ceb4b3a0bcda4f424dd
sashik mindaka <mindakaofficial0069@gmail.com>
2026-09-07 15:37:33 +0530

    create main hero fonts

--- my_profectional_portfolio/src/components/Hero.jsx
+++ my_profectional_portfolio/src/components/Hero.jsx
@@ -7,11 +7,11 @@ export default function HeroSection() {
     <section className='bg-[#040811] min-h-screen py-20 px-8 flex items-center justify-center'>
       
 
-      <div className='w-full max-w-6xl mx-auto flex flex-col lg:flex-row items-center justify-between gap-10'>
+      <div className='w-full max-w-[95%] mx-auto flex flex-col lg:flex-row items-center justify-between gap-10'>
         
    
         <div className='w-full lg:w-1/2'>
-          <h1 className='text-white text-4xl lg:text-8xl font-bold leading-tight'>
+          <h1 className='black-ops-one-regular tracking-wide text-white text-4xl lg:text-8xl leading-tight'>
             I solve problems. <br />
             I craft digital <br />
             experiences. <br />
@@ -20,12 +20,12 @@ export default function HeroSection() {
           </h1>
         </div>
 
-        <div className='w-full lg:w-1/2 flex justify-center lg:justify-end'>
+        <div className='w-full lg:w-1/2 flex lg:justify-end' >
           <img 
             src={myimg1} 
             alt="my image 1" 
   
-            className='w-72 lg:w-225 h-auto object-cover rounded-2xl shadow-lg border border-gray-800 animate-float'
+            className='w-72 lg:w-170 h-auto object-cover rounded-2xl animate-float'
           />
         </div>
 
