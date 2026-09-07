84f7a2b04e633852979c4b764d0d78876244fece
sashik mindaka <mindakaofficial0069@gmail.com>
2026-09-07 20:13:18 +0530

    update responsive

--- my_profectional_portfolio/src/components/ManifestoSection.jsx
+++ my_profectional_portfolio/src/components/ManifestoSection.jsx
@@ -13,30 +13,30 @@ export default function ManifestoSection() {
 
         <h1 className='font-sans font-black tracking-tighter text-5xl sm:text-5xl md:text-7xl lg:text-8xl xl:text-9xl text-amber-50 mt-4'>
 
-          <span>I BUILD</span>
-          <span className='text-[#0B0C0A] bg-[#d4ff33] px-4 py-1 lg:px-6 lg:py-2 inline-block'>FRONT.</span>
+          <span className='block md:inline'>I BUILD</span>
+          <span className='text-[#0B0C0A] bg-[#d4ff33] px-3 py-1 md:px-5 md:py-2 inline-block mt-2 md:mt-0'>FRONT.</span>
 
         </h1>
         
 
-        <h1 className='font-sans font-black tracking-tighter text-9xl pl-25 text-amber-50'>
+        <h1 className='font-sans font-black tracking-tighter text-5xl sm:text-5xl md:text-7xl lg:text-8xl xl:text-9xl text-amber-50 mt-4'>
 
-          <span>I BUILD</span>
+          <span className='block md:inline'> I BUILD</span>
           <span className='text-[#0B0C0A] bg-[#86efac] px-4 '>BACK.</span>
 
         </h1>
 
 
-        <h1 className='font-sans font-black tracking-tighter text-9xl pl-25 text-amber-50'>
+        <h1  className='font-sans font-black tracking-tighter text-5xl sm:text-5xl md:text-7xl lg:text-8xl xl:text-9xl text-amber-50 mt-4'>
 
-          <span>I TEST </span>
-          <span className='text-[#0B0C0A] bg-[#fbbf24] px-4 lg:px-6 lg:py-2 inline-block'>EVERYTHING.</span>
+          <span className='block md:inline'> I TEST </span>
+          <span className='text-[#0B0C0A] bg-[#fbbf24] px-3 py-1 md:px-5 md:py-2 inline-block mt-2 md:mt-0 '>EVERYTHING.</span>
 
         </h1>
 
-        <p className='  text-gray-400 font-mono text-sm lg:text-base leading-relaxed ml-25 text-xl pt-20 text-left max-w-4xl pb-20'>
-          "I am Sashik Mindaka, a Software Engineering undergraduate at NSBM Green University. Why rely on a distributed team when one focused engineer can connect all the dots? From crafting React frontends to architecting Java Spring Boot APIs and MySQL databases, the entire ecosystem rests in a single pair of hands. Whether building platforms like TravelMania or complex management systems, I handle the full lifecycle. No handoffs, no communication silos—just complete, end-to-end digital experiences built with an engineering mindset."
-        </p>
+        <p className='text-gray-400 font-mono text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl leading-relaxed text-left max-w-4xl pt-10 md:pt-16'>
+  "I am Sashik Mindaka, a Software Engineering undergraduate at NSBM Green University. Why rely on a distributed team when one focused engineer can connect all the dots? From crafting React frontends to architecting Java Spring Boot APIs and MySQL databases, the entire ecosystem rests in a single pair of hands. Whether building platforms like TravelMania or complex management systems, I handle the full lifecycle. No handoffs, no communication silos—just complete, end-to-end digital experiences built with an engineering mindset."
+</p>
         
 
       </div>
