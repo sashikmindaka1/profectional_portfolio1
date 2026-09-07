92f5d44c854c65e4015d1283e91c365a13aa50a9
sashik mindaka <mindakaofficial0069@gmail.com>
2026-09-08 02:12:51 +0530

    finishing in skills page

--- my_profectional_portfolio/src/components/AboutLanguages.jsx
+++ my_profectional_portfolio/src/components/AboutLanguages.jsx
@@ -2,22 +2,23 @@ import React from 'react'
 
 export default function AboutLanguages() {
   return (
-    <section className='bg-amber-100'> 
-      <div className='justify-between'>
+    <section className='bg-amber-100 pl-4 lg:pl-8'> 
+      <div className='justify-between '>
         <h1>
-          <span className='font-sans font-black tracking-tighter text-5xl sm:text-5xl md:text-7xl lg:text-8xl xl:text-9xl text-black mt-4'>WHAT I WORK WITH</span>
-          
+          <span className='font-sans font-black tracking-tighter text-5xl sm:text-5xl md:text-7xl lg:text-8xl xl:text-9xl text-black pt-24 inline-block '>WHAT I WORK WITH</span>
+
           <span >// LANGUAGES AND TOOLS</span>
+           <p className='border-b-2 border-dashed border-black py-4'></p>
           </h1>
 
-          <div className='bg-[#d4ff33] flex flex-col lg:flex-row flex justify-between'>
+          <div className='bg-amber-100 hover:bg-[#d4ff33] flex flex-col lg:flex-row justify-between transition-all duration-300 ease-in-out hover:scale-[1.01] hover:shadow-2xl cursor-pointer'>
 
             <div>
 
               <span className=' font-sans font-black tracking-tighter text-2xl sm:text-2xl md:text-5xl lg:text-5xl xl:text-5xl text-black mt-4'>01</span>
-            <span className='font-sans font-black tracking-tighter text-5xl sm:text-5xl md:text-7xl lg:text-8xl xl:text-9xl text-black mt-4'>FRONTEND</span>
+            <span className='font-sans font-black tracking-tighter text-5xl sm:text-5xl md:text-7xl lg:text-7xl xl:text-8xl text-black mt-4'>FRONTEND</span>
 
-            <p className='text-black font-mono text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl leading-relaxed text-left max-w-4xl pt-10 md:pt-5 pl-13'>Crafting fast, responsive, and dynamic user interfaces. Highly familiar with the React and Vite ecosystems.</p>
+            <p className='text-black font-mono text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl leading-relaxed text-left max-w-4xl pt-10 md:pt-5 pl-13'>"Crafting fast, responsive, and dynamic user interfaces. Highly familiar with the React and Vite ecosystems."</p>
 
             </div>
 
@@ -27,8 +28,8 @@ export default function AboutLanguages() {
 
                   <button  className='border-2 bg-amber-50 rounded-full px-2 py-1 lg:px-6 lg:py-2 hover:bg-blue-950 hover:text-amber-50 transition-colors duration-300 '>  React  </button>
                   <button  className='border-2 bg-amber-50 rounded-full px-2 py-1 lg:px-6 lg:py-2 hover:bg-blue-950 hover:text-amber-50 transition-colors duration-300 '>vite</button>
-                  <button className='border-2 bg-amber-50 rounded-full px-2 py-1 lg:px-6 lg:py-2 hover:bg-blue-950 hover:text-amber-50 transition-colors duration-300 '>tailwind css</button>
-                  <button className='border-2 bg-amber-50 rounded-full px-2 py-1 lg:px-6 lg:py-2 hover:bg-blue-950 hover:text-amber-50 transition-colors duration-300 '>next Js</button>
+                  <button className='border-2 bg-amber-50 rounded-full px-2 py-1 lg:px-6 lg:py-2 hover:bg-blue-950 hover:text-amber-50 transition-colors duration-300 '>Tailwind CSS</button>
+                  <button className='border-2 bg-amber-50 rounded-full px-2 py-1 lg:px-6 lg:py-2 hover:bg-blue-950 hover:text-amber-50 transition-colors duration-300 '>Next.js</button>
             </span>
           
 
@@ -37,7 +38,135 @@ export default function AboutLanguages() {
 
           </div>
 
-      </div>
+      </ div>
+
+
+
+
+
+
+      <div className='justify-between '>
+        <>
+          
+           <p className='border-b-2 border-dashed border-black'></p>
+          </>
+
+          <div className='bg-amber-100 hover:bg-[#d4ff33] flex flex-col lg:flex-row justify-between transition-all duration-300 ease-in-out hover:scale-[1.01] hover:shadow-2xl cursor-pointer'>
+
+            <div>
+
+              <span className=' font-sans font-black tracking-tighter text-2xl sm:text-2xl md:text-5xl lg:text-5xl xl:text-5xl text-black mt-4'>02</span>
+            <span className='font-sans font-black tracking-tighter text-5xl sm:text-5xl md:text-7xl lg:text-7xl xl:text-8xl text-black mt-4'>BACKEND</span>
+
+            <p className='text-black font-mono text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl leading-relaxed text-left max-w-4xl pt-10 md:pt-5 pl-13'>"Architecting robust, secure, and scalable server-side systems and enterprise-grade RESTful APIs leveraging Java and Spring Boot."</p>
+
+            </div>
+
+            <div>
+
+              <span className='flex gap-4 lg:mt-15 lg:mr-30'>
+
+                  <button  className='border-2 bg-amber-50 rounded-full px-2 py-1 lg:px-6 lg:py-2 hover:bg-blue-950 hover:text-amber-50 transition-colors duration-300 '>  Java  </button>
+                  <button  className='border-2 bg-amber-50 rounded-full px-2 py-1 lg:px-6 lg:py-2 hover:bg-blue-950 hover:text-amber-50 transition-colors duration-300 '>Spring Boot</button>
+                  <button className='border-2 bg-amber-50 rounded-full px-2 py-1 lg:px-6 lg:py-2 hover:bg-blue-950 hover:text-amber-50 transition-colors duration-300 '>REST API</button>
+                  <button className='border-2 bg-amber-50 rounded-full px-2 py-1 lg:px-6 lg:py-2 hover:bg-blue-950 hover:text-amber-50 transition-colors duration-300 '>Maven</button>
+            </span>
+          
+
+            </div>
+
+
+          </div>
+
+      </ div>
+
+
+
+
+
+
+      <div className='justify-between '>
+        <>
+          
+           <p className='border-b-2 border-dashed border-black'></p>
+          </>
+
+          <div className='bg-amber-100 hover:bg-[#d4ff33] flex flex-col lg:flex-row justify-between transition-all duration-300 ease-in-out hover:scale-[1.01] hover:shadow-2xl cursor-pointer'>
+
+            <div>
+
+              <span className=' font-sans font-black tracking-tighter text-2xl sm:text-2xl md:text-5xl lg:text-5xl xl:text-5xl text-black mt-4'>03</span>
+            <span className='font-sans font-black tracking-tighter text-5xl sm:text-5xl md:text-7xl lg:text-7xl xl:text-8xl text-black mt-4'>DATABASE</span>
+
+            <p className='text-black font-mono text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl leading-relaxed text-left max-w-4xl pt-10 md:pt-5 pl-13'>"Designing efficient and optimized data architectures. Experienced in managing both relational and non-relational databases including MySQL, PostgreSQL, and MongoDB."</p>
+
+            </div>
+
+            <div>
+
+              <span className='flex gap-4 lg:mt-15 lg:mr-30'>
+
+                  <button  className='border-2 bg-amber-50 rounded-full px-2 py-1 lg:px-6 lg:py-2 hover:bg-blue-950 hover:text-amber-50 transition-colors duration-300 '>  MySQL  </button>
+                  <button  className='border-2 bg-amber-50 rounded-full px-2 py-1 lg:px-6 lg:py-2 hover:bg-blue-950 hover:text-amber-50 transition-colors duration-300 '>PostgreSQL</button>
+                  <button className='border-2 bg-amber-50 rounded-full px-2 py-1 lg:px-6 lg:py-2 hover:bg-blue-950 hover:text-amber-50 transition-colors duration-300 '>MongoDB</button>
+                  <button className='border-2 bg-amber-50 rounded-full px-2 py-1 lg:px-6 lg:py-2 hover:bg-blue-950 hover:text-amber-50 transition-colors duration-300 '>SQL / NoSQL</button>
+            </span>
+          
+
+            </div>
+
+
+          </div>
+
+
+
+
+
+
+          <div className='justify-between '>
+        <>
+          
+           <p className='border-b-2 border-dashed border-black'></p>
+          </>
+
+          <div className='bg-amber-100 hover:bg-[#d4ff33] flex flex-col lg:flex-row justify-between transition-all duration-300 ease-in-out hover:scale-[1.01] hover:shadow-2xl cursor-pointer'>
+
+            <div>
+
+              <span className=' font-sans font-black tracking-tighter text-2xl sm:text-2xl md:text-5xl lg:text-5xl xl:text-5xl text-black mt-4'>04</span>
+            <span className='font-sans font-black tracking-tighter text-5xl sm:text-5xl md:text-7xl lg:text-7xl xl:text-8xl text-black mt-4'>TESTING</span>
+
+            <p className='text-black font-mono text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl leading-relaxed text-left max-w-4xl pt-10 md:pt-5 pl-13'>"Ensuring seamless API performance and code reliability through rigorous validation and endpoint testing using Postman, JUnit, and Swagger."</p>
+
+            </div>
+
+            <div>
+
+              <span className='flex gap-4 lg:mt-15 lg:mr-30'>
+
+                  <button  className='border-2 bg-amber-50 rounded-full px-2 py-1 lg:px-6 lg:py-2 hover:bg-blue-950 hover:text-amber-50 transition-colors duration-300 '>  Postman  </button>
+                  <button  className='border-2 bg-amber-50 rounded-full px-2 py-1 lg:px-6 lg:py-2 hover:bg-blue-950 hover:text-amber-50 transition-colors duration-300 '>JUnit</button>
+                  <button className='border-2 bg-amber-50 rounded-full px-2 py-1 lg:px-6 lg:py-2 hover:bg-blue-950 hover:text-amber-50 transition-colors duration-300 '>Swagger</button>
+                  <button className='border-2 bg-amber-50 rounded-full px-2 py-1 lg:px-6 lg:py-2 hover:bg-blue-950 hover:text-amber-50 transition-colors duration-300 '>Jest</button>
+            </span>
+          
+
+            </div>
+
+
+          </div>
+
+      </ div>
+
+
+      </ div>
+
+
+
+
+
+
+
     </section>
   )
 }
